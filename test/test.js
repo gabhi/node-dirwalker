8539093f4be6f1468957ffdb6df113a450fc9b4d
Tony Pujals <tony@subfuzion.com>
2015-01-12 04:50:39 -0800

    fixed relname to include start directory

--- test/test.js
+++ test/test.js
@@ -78,11 +78,11 @@ function compareWalk(dir, options, done) {
         .on('entry', function (entry) {
           // entry.relname is the relative pathname starting from source dir;
           // this makes it easy to compare actual with expected entries
-          //console.log('%s', entry.relname);
+          console.log('%s', entry.relname);
           walker.results.push(entry.relname);
         })
         .on('error', function (err) {
-          console.log(err);
+          console.error(err);
           walker.errors.push(err);
         })
         .on('close', function () {
